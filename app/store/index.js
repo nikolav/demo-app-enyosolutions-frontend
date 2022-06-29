781d3cbf1fe63a935275be064d6dc78e506b049c
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-06-29 20:37:37 +0200

    heroku build

--- app/store/index.js
+++ app/store/index.js
@@ -6,8 +6,8 @@ import { stripEndSlashes } from "../../src/util";
 const API_URL_dev = "http://localhost:3031/api";
 const API_URL_production = "https://demo-enyosolutions.herokuapp.com/api/";
 //
-const API_URL = stripEndSlashes(API_URL_dev);
-// const API_URL = stripEndSlashes(API_URL_production);
+// const API_URL = stripEndSlashes(API_URL_dev);
+const API_URL = stripEndSlashes(API_URL_production);
 ////
 export {
   useAppData,
