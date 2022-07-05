b57affd00379cd7ba726148681f72a27462caf52
nikolav <admin@nikolav.rs>
2022-07-05 12:38:25 +0100

    minor refactors

--- app/store/index.js
+++ app/store/index.js
@@ -1,4 +1,10 @@
-import { useAppData, ADMIN, TEST, DISPAY_ARTICLE, APP_NOTIFICATION } from "./slice-appdata";
+import {
+  useAppData,
+  ADMIN,
+  TEST,
+  DISPAY_ARTICLE,
+  APP_NOTIFICATION,
+} from "./slice-appdata";
 import { useFlags, FLAG_TEST, FLAG_FEEDS_LOADING } from "./slice-flags";
 import { useArticles } from "./resource";
 import { stripEndSlashes } from "../../src/util";
@@ -6,8 +12,8 @@ import { stripEndSlashes } from "../../src/util";
 const API_URL_dev = "http://localhost:3031/api";
 const API_URL_production = "https://demo-enyosolutions.herokuapp.com/api/";
 //
-// const API_URL = stripEndSlashes(API_URL_dev);
-const API_URL = stripEndSlashes(API_URL_production);
+const API_URL = stripEndSlashes(API_URL_dev);
+// const API_URL = stripEndSlashes(API_URL_production);
 ////
 export {
   useAppData,
@@ -20,5 +26,5 @@ export {
   DISPAY_ARTICLE,
   FLAG_FEEDS_LOADING,
   FLAG_TEST,
-  TEST
+  TEST,
 };
